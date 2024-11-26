c69fcd064667d67960e2003c27b255d427a7c3d1
shaihi <shaihi@icloud.com>
2024-11-26 17:54:52 +0100

    כבר רשמתי את הקוד והדיאלוג בחירת זמן עובד

---
+++ app/src/main/java/com/shaihi/timepicker/MainActivity.java
@@ -0,0 +1,64 @@
+package com.shaihi.timepicker;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTime;
+    Button btnPickTime;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tvTime = findViewById(R.id.tvTime);
+        btnPickTime = findViewById(R.id.btnTime);
+
+        btnPickTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
+    }
+
+    private void showTimePickerDialog(){
+        Calendar calendar = Calendar.getInstance();
+        int minutes = calendar.get(Calendar.MINUTE);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                MainActivity.this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String text = String.format("%02d:%02d",hourOfDay,minute);
+                        tvTime.setText(text);
+                    }
+                },
+                minutes,
+                hour,
+                true
+        );
+        timePickerDialog.show();
+    }
+}
